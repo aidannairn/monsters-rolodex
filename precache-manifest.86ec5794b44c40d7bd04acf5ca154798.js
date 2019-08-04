8217f3031eb892b9b18d273c95c24ce96f62d3d3
aidannairn <aidan.nairn95@gmail.com>
2019-08-04 19:35:35 +1200

    Updates

---
+++ precache-manifest.86ec5794b44c40d7bd04acf5ca154798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93df59777a18d0c78a7f7daf1ab87ec3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "985dd2efbc2d405a2b75",
+    "url": "/monsters-rolodex/static/css/main.bceeb5a8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "985dd2efbc2d405a2b75",
+    "url": "/monsters-rolodex/static/js/main.1d9a6a8e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
